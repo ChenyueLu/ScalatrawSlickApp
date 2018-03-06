34229b7ef8a2d989d6f995fff57080bf0eeb38b7
Chenyue Lu <luchenyue1994@gmail.com>
2018-03-06 10:57:11 +0800

    Add Try Catch in code

--- src/main/scala/com/example/controllers/UserController.scala
+++ src/main/scala/com/example/controllers/UserController.scala
@@ -9,6 +9,7 @@ import org.json4s._
 import com.example.models.UserData
 import com.example.services.UserStore
 import com.example.commons.Constants._
+import scala.util._
 
 
 class UserController extends BaseController with FutureSupport {
@@ -25,14 +26,20 @@ class UserController extends BaseController with FutureSupport {
 
   get("/byId/:userId") {
     params.getAs[Int]("userId") match{
-      case Some(userId) => UserStore.getUserById(userId)
+      case Some(userId) => UserStore.getUserById(userId) match{
+        case Success(result) => result
+        case Failure(ex) => BadRequest(Map(MESSAGE_KEY -> ex.getMessage))
+      }
       case None => BadRequest(Map(MESSAGE_KEY -> MANDATORY_PARAM_IS_MISSING))}
   }
 
 
   get("/byName/:userName") {
     params.getAs[String]("userName") match{
-      case Some(userName) => UserStore.getUserByName(userName)
+      case Some(userName) => UserStore.getUserByName(userName) match{
+        case Success(result) => result
+        case Failure(ex) => BadRequest(Map(MESSAGE_KEY -> ex.getMessage))
+      }
       case None => BadRequest(Map(MESSAGE_KEY -> MANDATORY_PARAM_IS_MISSING))
     }
   }
@@ -40,7 +47,10 @@ class UserController extends BaseController with FutureSupport {
   post("/") {
 
     parsedBody.extractOpt[UserData] match{
-      case Some(userData) => UserStore.addUser(userData.name, userData.age.getOrElse[Int](0))
+      case Some(userData) => UserStore.addUser(userData.name, userData.age.getOrElse[Int](0)) match{
+        case Success(result) => result
+        case Failure(ex) => BadRequest(Map(MESSAGE_KEY -> ex.getMessage))
+      }
       case None => BadRequest(Map(MESSAGE_KEY -> MANDATORY_POST_DATA_MISSING))
     }
   }
@@ -53,10 +63,11 @@ class UserController extends BaseController with FutureSupport {
         parsedBody.extractOpt[UserData] match {
 
           case Some(userData) => UserStore.updateUser(userId, userData.name, userData.age.getOrElse(0)) match {
-
-            case true => Ok(Map(MESSAGE_KEY -> UPDATE_SUCCEED))
-
-            case false => Conflict(Map(MESSAGE_KEY -> UPDATE_FAILED))
+            case Success(result) => result match{
+              case true => Ok(Map(MESSAGE_KEY -> UPDATE_SUCCEED))
+              case false => Conflict(Map(MESSAGE_KEY -> UPDATE_FAILED))
+            }
+            case Failure(ex) => BadRequest(Map(MESSAGE_KEY -> ex.getMessage))
           }
 
           case None => BadRequest(Map(MESSAGE_KEY -> MANDATORY_POST_DATA_MISSING))
@@ -74,10 +85,11 @@ class UserController extends BaseController with FutureSupport {
       case Some(id) => {
 
         UserStore.deleteUser(id) match {
-
-          case true => Ok(Map(MESSAGE_KEY -> DELETE_SUCCEED))
-
-          case false => Conflict(Map(MESSAGE_KEY -> DELETE_FAILED))
+          case Success(result) => result match{
+            case true => Ok(Map(MESSAGE_KEY -> DELETE_SUCCEED))
+            case false => Conflict(Map(MESSAGE_KEY -> DELETE_FAILED))
+          }
+          case Failure(ex) => BadRequest(Map(MESSAGE_KEY -> ex.getMessage))
         }
       }
 
@@ -91,10 +103,11 @@ class UserController extends BaseController with FutureSupport {
       case Some(name) => {
 
         UserStore.deleteUserByName(name) match {
-
-          case true => Ok(Map(MESSAGE_KEY -> DELETE_SUCCEED))
-
-          case false => Conflict(Map(MESSAGE_KEY -> DELETE_FAILED))
+          case Success(result) => result match{
+            case true => Ok(Map(MESSAGE_KEY -> DELETE_SUCCEED))
+            case false => Conflict(Map(MESSAGE_KEY -> DELETE_FAILED))
+          }
+          case Failure(ex) => BadRequest(Map(MESSAGE_KEY -> ex.getMessage))
         }
       }
 
--- src/main/scala/com/example/services/UserStore.scala
+++ src/main/scala/com/example/services/UserStore.scala
@@ -3,46 +3,53 @@ package com.example.services
 import com.example.models.slick.User
 import com.example.models.UserDetails
 
+import scala.concurrent.Future
+import scala.util.Try
+
 
 object UserStore {
 
-  def createUserTable(): Unit = {
-    User.creatTable()
+  def createUserTable(): Try[Unit] = {
+    Try(User.creatTable())
   }
 
-  def initUserTable(): Unit = {
-    User.initTable()
+  def initUserTable(): Try[Unit] = {
+    Try(User.initTable())
   }
 
-  def addUser(name: String, age: Int): Option[UserDetails] = {
-    User.insert(name, age) map{
+  def addUser(name: String, age: Int): Try[Option[UserDetails]] = {
+    Try({
+      User.insert(name, age) map {
+        ana => UserDetails(ana.id.get, ana.name, ana.age)
+      }
+    })
+  }
+
+  def getUserById(id: Int): Try[Option[User]] = {
+    Try({
+      User.get(Some(id), None)
+    })
+  }
+
+  def getUserByName(name: String): Try[Option[User]] = {
+    Try(User.get(None, Some(name)))
+  }
+
+  def getAllUsers: Try[List[UserDetails]] = {
+    Try(User.all() map{
       ana => UserDetails(ana.id.get, ana.name, ana.age)
-    }
+    })
   }
 
-  def getUserById(id: Int): Option[User] = {
-    User.get(Some(id), None)
+  def deleteUser(id: Int): Try[Boolean] = {
+    Try(User.delete(Some(id), None))
   }
 
-  def getUserByName(name: String): Option[User] = {
-    User.get(None, Some(name))
+  def deleteUserByName(name: String): Try[Boolean] = {
+    Try(User.delete(None, Some(name)))
   }
 
-  def getAllUsers: List[UserDetails] = {
-    User.all() map{
-      ana => UserDetails(ana.id.get, ana.name, ana.age)
-    }
-  }
-
-  def deleteUser(id: Int): Boolean = {
-    User.delete(Some(id), None)
-  }
-
-  def deleteUserByName(name: String): Boolean = {
-    User.delete(None, Some(name))
-  }
-
-  def updateUser(id: Int, name: String, age: Int): Boolean = {
-    User.update(id, name, age)
+  def updateUser(id: Int, name: String, age: Int): Try[Boolean] = {
+    Try(User.update(id, name, age))
   }
 }
